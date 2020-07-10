ddba7fc6d103f03b010a34fdbe86a899c14c2dda
dfernandez <daniel.fernandez.perez@gmail.com>
2020-07-10 15:13:42 +0200

    Update RNAseq docker

--- RNA-seq/build_test.sh
+++ RNA-seq/build_test.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 IMAGE_VERSION=$(date '+%d%m%y')
-docker build --no-cache -t dfernand/rnaseq-snakemake:${IMAGE_VERSION} -t dfernand/rnaseq-snakemake:latest . && \
+docker build -t dfernand/rnaseq-snakemake:${IMAGE_VERSION} -t dfernand/rnaseq-snakemake:latest . && \
 docker push dfernand/rnaseq-snakemake:${IMAGE_VERSION}
 docker push dfernand/rnaseq-snakemake:latest
--- RNA-seq/install_packages_bioc.R
+++ RNA-seq/install_packages_bioc.R
@@ -1,13 +1,13 @@
 # bioconductor
-BiocManager::install(c( \
-        'org.Mm.eg.db', \
-        'org.Hs.eg.db', \
-        'DESeq2', \
-        'clusterProfiler', \
-        'pcaExplorer', \
-        'ReactomePA', \
-        'IHW', \
-        'apeglm', \
-        'tximport', \
+BiocManager::install(c(
+        'org.Mm.eg.db',
+        'org.Hs.eg.db',
+        'DESeq2',
+        'clusterProfiler',
+        'pcaExplorer',
+        'ReactomePA',
+        'IHW',
+        'apeglm',
+        'tximport',
         'rhdf5',
 		'GenomicFeatures'))
